a78c183519fb51d7256f8a90f3d456e8eea7cb7a
BiKen <biken@okvip.com>
2023-08-28 19:13:02 +0700

    publish

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import Home from '~/pages/Home';
 
-const publicRoutes = [{ path: '/', component: Home }];
+const publicRoutes = [
+    { path: '/https://bikenokvip.github.io/789bet-pc/', component: Home },
+];
 
 const privateRoutes = [];
 
